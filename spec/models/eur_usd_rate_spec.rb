80b92fd8e7e0edcf690527e2a39ca3bf4dc44433
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-10-21 00:28:48 +0300

    Install rspec. Add tests for model.

--- spec/models/eur_usd_rate_spec.rb
+++ spec/models/eur_usd_rate_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe EurUsdRate do
-  
+  let(:time_currency) { EurUsdRate.new(date: "2016-10-21", currency: "1.5000") }
+
+  subject { time_currency }
+
+  it { should respond_to(:date) }
+  it { should respond_to(:currency) }
 end
