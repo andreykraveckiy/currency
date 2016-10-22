7379705393d79c282d5f84df41bffba1c6fa28a7
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-10-22 13:49:40 +0300

    Correct test for Loader

--- spec/services/rates_reload_service_rspec.rb
+++ spec/services/rates_reload_service_rspec.rb
@@ -5,26 +5,30 @@ require 'csv'
 LOCAL_CSV = './spec/assets/data.csv'
 
 describe RatesReloadService do
-	before do
-		@test_instance = RatesReloadService.new
-		@test_instance.perform 
-		@array = CSV.read(open(LOCAL_CSV))[HEADER_SIZE..-1]
-	end
-
-	it "should include data from local csv" do
-		expect(@array - @test_instance.data).to be_empty 
-	end
-
 	describe "work with database" do
-		let (:temp) { EurUsdRate.find_by(date: @array[0][0]) }
 
-		it "should not be nil" do
-			expect(temp).to_not be_nil
+		describe "before loading" do
+			it "should have empty db" do
+				expect(EurUsdRate.count).to eq 0
+			end
 		end
 
-		it "should have same date and currency" do
-			expect(temp.date).to eq @array[0][0]
-			expect(temp.currency.to_f).to eq @array[0][-1].to_f
+		describe "after loading" do
+			before do
+				@test_instance = RatesReloadService.new(LOCAL_CSV)
+				@test_instance.perform 
+			end
+
+			let (:temp) { EurUsdRate.find_by(date: @test_instance.data[0][0]) }
+
+			it "should not be nil" do
+				expect(temp).to_not be_nil
+			end
+
+			it "should have same date and currency" do
+				expect(temp.date).to eq @test_instance.data[0][0]
+				expect(temp.currency.to_f).to eq @test_instance.data[0][-1].to_f
+			end
 		end
 	end	
 end
